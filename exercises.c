018b8ed36070735613aca858b9737c00684534cf
Your Name <you@example.com>
2022-03-12 23:29:28 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -120,7 +120,7 @@ actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
    int i;
-   for(i=0;a->datos[i]!=NULL;i++){
+   for(i=0;(a->datos[i])!=NULL;i++){
       c->datos[i]=(a->datos[i])+(b->datos[i]);
    }
 }
